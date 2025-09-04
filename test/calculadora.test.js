92aac8c704fbc4dd00a5fe62663f3351b71c1b74
Michelle <michelleyonezawa@gmail.com>
2025-09-04 11:47:26 -0300

    Adicionei testes automatizados para a função

---
+++ test/calculadora.test.js
@@ -0,0 +1,17 @@
+const { somarDoisNumeros } = require('../src/calculadora')
+const {expect} = require('chai');
+
+describe('Testes de soma de dois números', function() {
+    it('Função capaz de somar dois números positivos', function() {
+        // Coletar resultado da soma
+        const resultadoDaSoma = somarDoisNumeros(5, 3);
+        // Comparar com o resultado que você espera
+        expect(resultadoDaSoma).to.equal(8);
+    })
+    it('Função capaz de somar um número positivo e um negativo', function() {
+        // Coletar resultado da soma
+        const resultadoDaSoma = somarDoisNumeros(50, -15);
+        // Comparar com o resultado que você espera
+        expect(resultadoDaSoma).to.equal(35);
+    });
+});
