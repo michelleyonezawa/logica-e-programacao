364d59df6d5b10b762d976433d905d4ffc81f8a7
Michelle <michelleyonezawa@gmail.com>
2025-09-06 10:44:02 -0300

    Adicionei duas funções novas, sendo elas somar dois números 0 e somar dois números negativos

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -7,11 +7,24 @@ describe('Testes de soma de dois números', function() {
         const resultadoDaSoma = somarDoisNumeros(5, 3);
         // Comparar com o resultado que você espera
         expect(resultadoDaSoma).to.equal(8);
-    })
+    });
     it('Função capaz de somar um número positivo e um negativo', function() {
         // Coletar resultado da soma
         const resultadoDaSoma = somarDoisNumeros(50, -15);
         // Comparar com o resultado que você espera
         expect(resultadoDaSoma).to.equal(35);
     });
+    it('Função capaz de somar dois números que sejam equivalentes a 0', function() {
+        // Coletar resultado da soma
+        const resultadoDaSoma = somarDoisNumeros(0, 0)
+        // Comparar com o resultado que você espera
+        expect(resultadoDaSoma).to.equal(0)
+    });
+    it('Função capaz de somar dois números negativos', function() {
+        // Coletar resultado da soma
+        const resultadoDaSoma = somarDoisNumeros(-15, -20)
+        // Comparar com o resultado que você espera
+        expect(resultadoDaSoma).to.equal(-35)
+    });
 });
+
